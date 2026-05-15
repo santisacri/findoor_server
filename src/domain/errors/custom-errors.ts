aa4c8fcb1d9ede5dd2858d9d40b6b773a5ba4e8e
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-05-15 19:11:38 -0300

    chore(errors): add suport to prisma errors

--- src/domain/errors/custom-errors.ts
+++ src/domain/errors/custom-errors.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "../../../generated/prisma/client"
+
 
 
 export class CustomError extends Error {
@@ -6,7 +8,7 @@ export class CustomError extends Error {
         public readonly message: string,
         public readonly statusCode: number
     ) { super(message) }
-    
+
 
     static badRequest(message = "Bad Request") {
         return new CustomError(message, 400)
@@ -26,5 +28,25 @@ export class CustomError extends Error {
 
     static internal(message = "Internal Server Error") {
         return new CustomError(message, 500)
+    }
+
+    static fromPrisma(error: unknown): never {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            switch (error.code) {
+                case 'P2002':
+                    const message = error.message.split('\n').at(-1)?.trim();
+                    throw new CustomError(  message ?? 'Unique constraint violated', 409);
+                case 'P2025':
+                    throw new CustomError('Record not found', 404);
+                case 'P2003':
+                    throw new CustomError('Foreign key violation', 400);
+            }
+        }
+
+        if (error instanceof Prisma.PrismaClientValidationError) {
+            throw new CustomError('Invalid data sent to database', 400);
+        }
+
+        throw new CustomError('Internal Server Error', 500);
     }
 }
