d1ca362125b2c92689120d881aa8637ed106672f
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-05-26 16:06:25 -0300

    fix: fix env schema

--- src/env.schema.ts
+++ src/env.schema.ts
@@ -9,7 +9,7 @@ const envSchema = z.object({
     CLOUDINARY_CLOUD_NAME: z.string(),
     CLOUDINARY_API_KEY: z.string(),
     CLOUDINARY_API_SECRET: z.string(),
-    FRONTEND_URL: z.url().optional()
+    FRONTEND_URL: z.string().optional()
 })
 
 export const envs = envSchema.parse(process.env)
