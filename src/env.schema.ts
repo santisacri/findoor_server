b285e5656b0824ef9df58ad40aceab47bd5fd1ed
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-05-28 21:00:25 -0300

    fix: fix frontend url prop on env schema

--- src/env.schema.ts
+++ src/env.schema.ts
@@ -11,7 +11,7 @@ const envSchema = z.object({
     CLOUDINARY_CLOUD_NAME: requiredString,
     CLOUDINARY_API_KEY: requiredString,
     CLOUDINARY_API_SECRET: requiredString,
-    FRONTEND_URL: z.string().default('http://localhost:3000'),
+    FRONTEND_URL: z.transform(value => (value === '' || value === undefined) ? 'http://localhost:5173' : value),
     RESEND_API_KEY: requiredString,
     DOMAIN: z.string().optional()
 })
